f1c2e7081bea4d86bf688cef372d0db4bc4923de
Eric Schles <ericschles@gmail.com>
2016-06-03 14:16:55 -0400

    updating

--- lecture10.py
+++ lecture10.py
@@ -1,16 +1,5 @@
 import random
 
-class TreeNode:
-    def __init__(self,value,color="white",parent=None,children=[],discovery_time=None,finish_time=None):
-        self.value = value
-        self.color = color
-        self.parent = parent
-        self.children = children
-        self.discovery_time = discovery_time
-        self.finish_time = finish_time
-    def __str__(self):
-        return repr(self.value)
-
 class GraphNode:
     def __init__(self,value,color="white",parent=None,discovery_time=None,finish_time=None):
         self.value = value
@@ -32,31 +21,23 @@ class Graph:
         self.internal[from_node].append(to_node)
 
         
-def depth_first_search(graph,tree,start_node,current_time):
-    cur = TreeNode(start_node)
-    cur.parent = tree
+def depth_first_search(graph,start_node,current_time,level,capture):
     start_node.color = "grey"
     start_node.discovery_time = current_time
     current_time += 1
+    try:
+        capture[level].append(start_node.value)
+    except:
+        capture[level] = [start_node.value]
     for vertex in graph.internal[start_node]:
-        cur.children.append(TreeNode(vertex))
         if vertex.color == "white":
             vertex.parent = start_node
-            current_time = depth_first_search(graph,cur,vertex,current_time)
+            current_time = depth_first_search(graph,vertex,current_time,level+1,capture)
             current_time += 1
     start_node.finish_time = current_time
     start_node.color = "black"
     return start_node.finish_time
-
-def print_tree(cur_node,seen_nodes):
-    if cur_node:
-        print(cur_node.value)
-        seen_nodes.append(cur_node)
-        print()
-        for elem in cur_node.children:
-            if elem in seen_nodes: continue
-            print_tree(elem,seen_nodes)
-
+            
 g = Graph()
 graph_nodes = []
 for i in range(20):
@@ -69,10 +50,13 @@ for i in range(20):
             g.add_connection(node,g_node)
     graph_nodes.append(node)
 
-
 start_node = graph_nodes[random.randint(0,len(graph_nodes)-1)]
-root = TreeNode(None)
-root.children.append(TreeNode(start_node))
-print(depth_first_search(g,root,start_node,0))
-print_tree(root,[])
-    
+capture = {}
+print("total time to end up back at start:",depth_first_search(g,start_node,0,0,capture))
+print("nodes in graph:",[elem.value for elem in g.internal.keys()])
+print("size of graph:",max([elem.value for elem in g.internal.keys()]))
+print("start node:",start_node.value)
+print("start node color:",start_node.color)
+print("traversal path:")
+for level in capture.keys():
+    print(level,capture[level])
